1783219bc304979116cc2b908495903cf05e24c7
James Richardson <iagowynne@gmail.com>
2021-07-09 17:41:51 +0100

    Basic discord login code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-function main() {
-    console.log("hello world");
-}
+import {Client} from "discord.js";
+import * as dotenv from "dotenv";
 
-main();
+dotenv.config();
+const client = new Client();
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user?.tag}`);
+});
+
+const token = process.env.TOKEN;
+console.log(token);
+
+client.login(process.env.TOKEN);
