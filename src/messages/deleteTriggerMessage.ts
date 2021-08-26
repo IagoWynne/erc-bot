b3095710c5be3d8bfc30b7dc0e5ce5d8e509d71e
IagoWynne <iagowynne@gmail.com>
2021-08-26 16:54:46 +0100

    Uat (#32)
    * Added channels partial (#25)
    
    * Added message embed splitter for very long messages (#29)
    
    * Bug/throttling rapid copy paste (#31)
    
    * Added message embed splitter for very long messages
    
    * Prevented bot attempting to delete the same trigger message twice

--- src/messages/deleteTriggerMessage.ts
+++ src/messages/deleteTriggerMessage.ts
@@ -5,8 +5,11 @@ import * as Discord from "../discord";
 const deleteTriggerMessage = async (message: Message) => {
   try {
     if (message.channel.type !== "DM") {
-      Discord.addDeletedMessageId(message.id);
-      await message.delete();
+      if (!Discord.isBotDeletedMessage(message.id)) {
+        Discord.addDeletedMessageId(message.id);
+
+        await message.delete();
+      }
     }
   } catch (e) {
     Log.error(e);
